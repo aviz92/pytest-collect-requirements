cb220f5f5caa0945af7fc29760740096d38f94de
avizaguri-macbook <avi210892@gmail.com>
2025-12-13 12:49:01 +0200

    Add pytest plugin to collect test requirements from external sources

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,7 +1,23 @@
-class TestDummy:
-    def test_dummy1(self) -> None:
-        assert 1 == 1  # pylint: disable=R0133,R0124
+from typing import Any
+
+import pytest
+from custom_python_logger import get_logger
+
+logger = get_logger(__name__)
 
 
-def test_dummy2() -> None:
+def requirements(**kwargs: Any) -> Any:
+    return pytest.mark.requirements(**kwargs)
+
+
+@pytest.mark.integration
+@requirements(cloud_instance="c5.large", region="eu-west-1")
+def test_requirements() -> None:
+    print()
+    assert 1 == 1  # pylint: disable=R0133,R0124
+
+
+@requirements(cloud_instance="c5.small", region="eu-west-2")
+def test_requirements2() -> None:
+    print()
     assert 1 == 1  # pylint: disable=R0133,R0124
